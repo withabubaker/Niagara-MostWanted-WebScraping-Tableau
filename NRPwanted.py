20a3cf10f47f659fe2116d7f699950827a6558a4
Mohammed <withabubaker@hotmail.com>
2023-09-30 21:23:18 -0400

    cleanup NPRwanted

--- NRPwanted.py
+++ NRPwanted.py
@@ -5,7 +5,7 @@ import csv
 source = requests.get('https://www.niagarapolice.ca/en/news-and-events/Niagara-s-Wanted.aspx').text
 soup = BeautifulSoup(source, 'lxml') # use lxml parser
 
-csv_file = open('NRP1.csv', 'w') 
+csv_file = open('NRP.csv', 'w') 
 csv_writer = csv.writer(csv_file) # write the result into csv file
 csv_writer.writerow(['Name', 'Age', 'Location', 'Crime', 'Date']) # these are the data we need to collect
 
@@ -15,9 +15,16 @@ for match in soup.find_all('tr',{"class":['row','altrow']}): # extract all text 
     try:
         #result = match.text.strip() 
         result = match.get_text(separator="\n").strip() #replace <br> with new line, this will help us to select items from info list
-        print(result)
+        #print(result)
         info = result.splitlines()
         name = info[0]
+        for i in range(1,1,5):
+            if info[i] != '':
+                year = info[i]
+                i+=1
+                break
+            else:
+                pass
         year = info [1]
         location = info [2]
         crime = info [3:-1]
@@ -25,12 +32,6 @@ for match in soup.find_all('tr',{"class":['row','altrow']}): # extract all text 
         print(info)
     except Exception as e:
         result = None
-        info = None
-        name = None
-        year = None
-        location = None
-        crime = None
-        date = None
     csv_writer.writerow([name,year,location,crime,date])
 print('Done')  
 csv_file.close()
